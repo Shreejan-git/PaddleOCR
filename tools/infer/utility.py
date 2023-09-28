7e75ef8462b29c67475ab9ab556a6ea82ade818e
shreejan <hustlingshreejan@gmail.com>
2023-09-28 09:58:08 +0545

    changed the detection algo to db++

--- tools/infer/utility.py
+++ tools/infer/utility.py
@@ -49,7 +49,7 @@ def init_args():
     # params for text detector
     parser.add_argument("--image_dir", type=str)
     parser.add_argument("--page_num", type=int, default=0)
-    parser.add_argument("--det_algorithm", type=str, default='DB')
+    parser.add_argument("--det_algorithm", type=str, default='DB++')  # changed to DB++
     parser.add_argument("--det_model_dir", type=str)
     parser.add_argument("--det_limit_side_len", type=float, default=960)
     parser.add_argument("--det_limit_type", type=str, default='max')
