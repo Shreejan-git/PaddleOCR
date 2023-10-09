13cf6a90afbb4aa64132a21c74bff0eed334d8a9
shreejan_stha <hustlingshreejan@gmail.com>
2023-10-09 21:55:05 +0545

    package structuring done

--- ocr/__init__.py
+++ ocr/__init__.py
@@ -1,8 +1,17 @@
-from paddle_ocr import *
-from paddle_structure import *
+from .paddle_ocr import *
+from .paddle_structure import *
+from .config import *
+from .utils import *
 
-__all__ = [
-    'PaddleOCR', 'PPStructure', 'draw_ocr', 'draw_structure_result',
-    'save_structure_res', 'download_with_progressbar', 'sorted_layout_boxes',
-    'convert_info_docx', 'to_excel'
-]
+"""
+This package contains the module's (paddle ocr and paddle structure) entry point and their utility functions.
+"""
+
+# __all__ = [
+#     'PaddleOCR', 'PPStructure', 'draw_ocr', 'draw_structure_result',
+#     'save_structure_res', 'download_with_progressbar', 'sorted_layout_boxes',
+#     'convert_info_docx', 'to_excel'
+#]
+
+# __all__ = [
+#     'PaddleOCR', 'PPStructure', 'draw_structure_result', 'download_with_progressbar', 'parse_args']
