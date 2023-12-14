39221c589f2800ac25a99df423817d605893ac61
Shreejan-git <hustlingshreejan@gmail.com>
2023-12-14 21:44:38 +0545

    add openai api

--- examples/text_det_rec_example.py
+++ examples/text_det_rec_example.py
@@ -10,6 +10,13 @@ from ppocr.utils.utility import get_image_file_list
 from ppocr.utils.logging import get_logger
 
 logger = get_logger()
+import os
+from langchain.chains import LLMChain, SimpleSequentialChain
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
+from getpass import getpass
+
+import json
 
 
 def paddle_det_and_rec(input_file: Optional[str] = None, det: bool = True, rec: bool = False):
@@ -49,15 +56,19 @@ def paddle_det_and_rec(input_file: Optional[str] = None, det: bool = True, rec: 
                 bboxes = []
                 img = page[0]
                 det_rec_results = page[1]
+                all_recognized_text = []
                 for data_in_each_page in det_rec_results:
                     bbox = data_in_each_page[0]
-                    rec_text = det_rec_results[1]
-                    print(rec_text)
+                    rec_text = data_in_each_page[1]
+                    # print(rec_text)
+                    all_recognized_text.append(rec_text[0])
                     bboxes.append(bbox)
                 visualize_bboxes(img=img, bboxes=bboxes, rec=rec)
-
-        # else:
-        #     visualize_bboxes(img=img, bboxes=bboxes, rec=rec)
+                # print(all_recognized_text)
+                # else:
+                #     visualize_bboxes(img=img, bboxes=bboxes, rec=rec)
+                print(all_recognized_text)
+                return all_recognized_text
 
 
 def visualize_bboxes(img: np.ndarray, bboxes: List, rec: bool = False):
@@ -82,11 +93,64 @@ def visualize_bboxes(img: np.ndarray, bboxes: List, rec: bool = False):
     cv2.waitKey(0)
 
 
+def llm_component(img_link):
+    prompt_text = paddle_det_and_rec(input_file=img_link, det=True, rec=True)
+    # prompt_text = """
+    # You are an expert admin who will extract core information from documents
+    #
+    # {recognized_text}
+    #
+    # Above is the content; please try to extract all data points from the content above and export and
+    # return in a JSON array format only. Do not auto generate the information. Only return what
+    # there is in the document
+    #
+    # """
+
+    prompt_template = PromptTemplate(
+        input_variables=['recognized_text'],
+
+        template="""
+    You are an expert admin who extracts core information from documents
+
+    {recognized_text}
+
+    Above is the content; please try to extract the following data:
+    1) Extract the details of the bank such as name, contact number, website and address (postbox, city, state, country,
+    zip).
+    2) Details of the client. Such as name, account no, statement start date, statement end date, account type, currency
+    type and address (postbox, city, state, country, zip).
+
+    Return in a JSON array format. Do not auto generate the information. Only return what is there in document or else
+    return an empty string like ""
+ """
+    )
+    llm = OpenAI(openai_api_key="sk-c9N3HG7fDbWoCUzHKpQjT3BBlbkFJhKwpXMhcT2Dr9R8qMGRBB")
+    llm_chain = LLMChain(llm=llm, prompt=prompt_template, verbose=True)
+
+    # sequential_chain = SimpleSequentialChain(chains=[llm_chain], input_variables=['text'], verbose=True)
+
+    response = llm_chain.run(prompt_text)
+    print(response)
+
+    returning_file = json.dumps(response[0])
+
+    return returning_file
+
+
 if __name__ == "__main__":
     # image_path = "/home/vertexaiml/Downloads/ocr_test_image/nepal.png"
-    image_path = "/home/vertexaiml/Downloads/ocr_test_image/4 page.jpg"
-    # image_path = "/home/vertexaiml/Downloads/ocr_test_image/blank_white.jpg"
-    file_path = "/home/vertexaiml/Downloads/Vertex_It/Poc_Sample/Bank_Of_America/Bank of America.pdf"
+    # image_path = "/home/vertexaiml/Downloads/ocr_test_image/4 page.jpg"
+    # image_path = "/home/vertexaiml/Downloads/ocr_test_image/testing.jpg"
+    # image_path = "/home/vertexaiml/Downloads/ocr_test_image/sample_Invoice.png"
+    # image_path = "/home/vertexaiml/Downloads/ocr_test_image/unstructured.png"
+    # image_path = "/home/vertexaiml/Downloads/ocr_test_image/unstructured_second.png"
+    # image_path = "/home/vertexaiml/Downloads/ocr_test_image/unstructured_third.png"
+    # image_path = "/home/vertexaiml/Downloads/ocr_test_image/unstructured_forth.png"
+    # image_path = "/home/vertexaiml/Downloads/ocr_test_image/second_table_data.png"
+    image_path = "/home/vertexaiml/Downloads/ocr_test_image/tabular_small.jpg"
+    # image_path = "/home/vertexaiml/Downloads/Vertex_It/Poc_Sample/Bank_Of_America/images/5.png"
 
     # paddle_det_and_rec(input_file=image_path, det=True, rec=True)
-    paddle_det_and_rec(input_file=file_path, det=True, rec=True)
+    paddle_det_and_rec(input_file=image_path, det=True, rec=True)
+    # llm_component(image_path)
+    # print(os.environ.get('OPENAI_API_KEY'))
